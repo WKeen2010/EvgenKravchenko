3b58b784e1f04f209c3709d11cad8888f9e28211
WKeen2010 <120595338+WKeen2010@users.noreply.github.com>
2023-02-26 11:42:37 +0200

    Add files via upload

---
+++ tmp.py
@@ -0,0 +1,11 @@
+class Animal():
+    def walk(self):
+        print("walking")
+    def danse(self):
+        print("DANSING!")
+    def eat(self):
+        print("Om-nom-nom")
+    def sleep(self):
+        print("Z-z-z")
+
+Mr_Cat = Animal()
